9f8cbaf08a374cead8246a1e3f6736efaf24d09d
Dong Chi <21130295@st.hcmuaf.edu.vn>
2024-10-10 09:39:42 +0700

    update

--- src/main/java/context/DBContext.java
+++ src/main/java/context/DBContext.java
@@ -5,15 +5,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBContext {
-//	
+
+	String url = "jdbc:sqlserver://DESKTOP-RDMJ7GI\\MSSQLSERVER02:1433;databaseName=DongHo;encrypt=false";
+	String userName = "sa";
+	String password = "123";
+
+// SQLSERVER name dunghoang: DESKTOP-9LNQ1HF
 //	String url = "jdbc:sqlserver://DESKTOP-9LNQ1HF\\MSSQLSERVER02:1433;databaseName=DongHo;encrypt=false";
 //	String userName = "sa";
 //	String password = "dunghoang168";
-
-// SQLSERVER name dunghoang: DESKTOP-9LNQ1HF
-	String url = "jdbc:sqlserver://DESKTOP-9LNQ1HF\\MSSQLSERVER02:1433;databaseName=DongHo;encrypt=false";
-	String userName = "sa";
-	String password = "dunghoang168";
 
 	
 	public Connection getConnection() throws Exception{
