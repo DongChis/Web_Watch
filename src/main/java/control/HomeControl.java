80b1b63d9ae8a30b12d7321695f8bf80106ce8aa
Dong Chi <21130295@st.hcmuaf.edu.vn>
2024-11-04 19:58:27 +0700

    init

--- src/main/java/control/HomeControl.java
+++ src/main/java/control/HomeControl.java
@@ -16,11 +16,6 @@ import entity.Product;
 @WebServlet(name = "HomeControl", urlPatterns = { "/home" })
 public class HomeControl extends HttpServlet {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
 	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTF-8");
