8aee5f14a2235e38007ab9d5e671b5f97f1817d2
Khang <21130394@st.hcmuaf.edu.vn>
2024-12-24 14:14:08 +0700

    fix phuong thuc load key o controller

--- LTW_tep/toolKy/src/controller/ChuKi_Control.java
+++ LTW_tep/toolKy/src/controller/ChuKi_Control.java
@@ -7,8 +7,11 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.security.KeyFactory;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 
 import javax.swing.JFileChooser;
@@ -246,8 +249,16 @@ public class ChuKi_Control {
 		if (returnValue == JFileChooser.APPROVE_OPTION) {
 			File file = fileChooser.getSelectedFile();
 			try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+		        String keyString = reader.readLine();
 				view.getTextPublicKey().setText(reader.readLine());
 				view.getTextOutput().setText("Public Key loaded from: " + file.getAbsolutePath());
+				 byte[] keyBytes = Base64.getDecoder().decode(keyString);
+			        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+			        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(keyBytes);
+			        PublicKey publicKey = keyFactory.generatePublic(keySpec);
+
+			        // Set public key vào model
+			        model.setPublicKey(publicKey);
 			} catch (IOException ex) {
 				view.getTextOutput().setText("Error loading public key: " + ex.getMessage());
 			}
@@ -255,12 +266,21 @@ public class ChuKi_Control {
 
 		// Load Private Key
 		fileChooser.setDialogTitle("Load Private Key");
-		returnValue = fileChooser.showOpenDialog(null);
+		 returnValue = fileChooser.showOpenDialog(null);
 		if (returnValue == JFileChooser.APPROVE_OPTION) {
 			File file = fileChooser.getSelectedFile();
 			try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+				String keyString = reader.readLine();
 				view.getTextPrivateKey().setText(reader.readLine());
 				view.getTextOutput().setText("Private Key loaded from: " + file.getAbsolutePath());
+				 // Chuyển đổi chuỗi private key sang đối tượng PrivateKey
+		        byte[] keyBytes = Base64.getDecoder().decode(keyString);
+		        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+		        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
+		        PrivateKey privateKey = keyFactory.generatePrivate(keySpec);
+
+		        // Set private key vào model
+		        model.setPrivateKey(privateKey);
 			} catch (IOException ex) {
 				view.getTextOutput().setText("Error loading private key: " + ex.getMessage());
 			}
